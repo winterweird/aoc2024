b6ffb1f2ea6d3b0cc1875038b5a6115b8766d9e5
Vegard Itland <vegard.itland@bluetree.no>
2024-12-01 09:38:07 +0100

    Robustness and formatting

--- run.rb
+++ run.rb
@@ -1,19 +1,37 @@
 #!/usr/bin/env ruby
 
+def executable?(path)
+  File.executable?(path) && !File.directory?(path)
+end
+
+def header(day)
+  [
+    " #{'='*78} ",
+    "|#{"Advent of Code 2024 --- Day #{day.to_i}".center(78)}|",
+    " #{'='*78} "
+  ].join("\n")
+end
+
 days = ARGV
 if days.empty?
   days = `ls .`.split(/\s+/).select { _1.match?(/\d+/) }
 end
-days.map! { _1.rjust(2, "0") }
+days = days.map { _1.rjust(2, "0") }
 
 days.each do |day|
-  puts "Advent of Code 2024 --- Day #{day.to_i}"
-  puts "-"*80
-  puts "Part 1:"
-  puts `cd #{day} && ./1.sh`
-  puts "-"*80
-  puts "Part 2:"
-  puts `cd #{day} && ./2.sh`
+  part1 = "#{day}/1.sh"
+  part2 = "#{day}/2.sh"
+
+  puts header(day)
+
+  if !executable?(part1) && !executable?(part2)
+    puts "\nSkipping Day #{day.to_i}"
+    next
+  end
+
+  puts "\nPart 1: #{executable?(part1) && `cd #{day} && ./1.sh` || "Skipping..."}"
+  puts "\n#{'-'*80}"
+  puts "\nPart 2: #{executable?(part2) && `cd #{day} && ./2.sh` || "Skipping..."}"
   puts
 end
 
