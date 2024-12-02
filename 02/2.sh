0ff5bca81e5b21c44b8d25ec65e5e8f554e8d61b
Vegard Itland <vegard.itland@bluetree.no>
2024-12-02 08:23:58 +0100

    Inline part 2

--- 02/2.sh
+++ 02/2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat puzzle.in | rb  $(cat 2.rb) | rb 'map(&:to_i).sum'
+cat puzzle.in | rb 'def check?(l); [[1], [-1]].include?(l.each_cons(2).map{|i,j| ((i-j).abs < 4 ? i-j : 0)<=>0}.uniq); end; def safe?(l); check?(l) || (0...l.length).any?{|i| check?(l.reject.with_index{|_,j| j == i})}; end; map { |l| l.split(/ +/).map(&:to_i) }.select{|l| safe?(l)}.length'
