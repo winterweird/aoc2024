f796aa93608295f6abfc1f1f96e1ca4290690f37
Vegard Itland <vegard.itland@bluetree.no>
2024-12-16 15:27:47 +0100

    Solve day 16 part 1 with help
    I kinda consider this a failure because I had to look at a reference solution to figure out where mine was wrong. I still don't know exactly why it failed at first :(

---
+++ 16/1.rb
@@ -0,0 +1,95 @@
+require "rubygems"
+require "algorithms"
+
+def getorient(dy,dx)
+  case [dy,dx]
+  when [1,0]
+    "S"
+  when [-1,0]
+    "N"
+  when [0,1]
+    "E"
+  when [0,-1]
+    "W"
+  end
+end
+def h(rp, ep)
+  a,b = rp
+  c,d = ep
+  (a-c).abs + (b-d).abs
+end
+def dijk(m, rp, ep)
+  s = Containers::MinHeap.new([[0, *rp, "E"]])
+  y,x = rp
+  knownscore = Array.new(m.length) { Array.new(m[0].length) { Float::INFINITY } }
+  knownscore[y][x] = 0
+
+  visited = Array.new(m.length) { Array.new(m[0].length) { {"N" => false, "W" => false, "E" => false, "S" => false} } }
+
+  camefrom = Array.new(m.length) { Array.new(m[0].length) { nil } }
+
+  while !s.empty?
+    cost, y,x, orient = s.pop
+
+    STDERR.puts "VISITED #{[y,x]} AT COST #{cost}#{[y,x]==ep ? ' AND TERMINATED': ''}"
+    next if visited[y][x][orient]
+    visited[y][x][orient] = true
+    if [y,x] == ep
+      return cost
+    end
+    knownscore[y][x] = cost
+
+    [[1,0],[-1,0],[0,1],[0,-1]].each do |dy,dx|
+      ny = y+dy
+      nx = x+dx
+      next if m[ny][nx] == "#"
+      neworient = getorient(dy,dx)
+      next if {"E" => "W", "W" => "E", "N" => "S", "S" => "N"}[neworient] == orient
+      g = knownscore[y][x] + 1 + (neworient != orient ? 1000 : 0)
+      s << [g, ny, nx, neworient] if !visited[ny][nx][orient]
+    end
+  end
+
+  y,x = ep
+  #cur=camefrom[y][x]
+  #ps = []
+  #while [cur[0],cur[1]] != rp
+  #  STDERR.puts cur.to_s
+  #  ps << cur
+  #  a,b,o = cur
+  #  cur = camefrom[a][b]
+  #end
+  #STDERR.puts ps.map(&:to_s)
+  #m2 = Marshal.load(Marshal.dump(m))
+  #ps.each do |a,b,o|
+  #  m2[a][b] = case o
+  #             when "N" then "^"
+  #             when "S" then "v"
+  #             when "E" then ">"
+  #             when "W" then "<"
+  #             end
+  #end
+  #STDERR.puts m2.map(&:join)
+  return knownscore[y][x]
+
+end
+
+
+m = eval(gets)
+rp = nil
+ep = nil
+m.each_with_index do |l,i|
+  l.each_with_index do |c,j|
+    rp = [i,j] if c == "S"
+    ep = [i,j] if c == "E"
+  end
+end
+
+
+
+STDERR.puts rp.to_s
+STDERR.puts ep.to_s
+STDERR.puts m.map(&:join)
+
+
+puts dijk(m, rp, ep)
